f051e4ac5c71b0d23e96c7b72cca4a590b663034
Nafla Diva Syafia <nafladiva@gmail.com>
2021-10-05 12:05:15 +0700

    add final score in result

--- lib/result.dart
+++ lib/result.dart
@@ -6,9 +6,11 @@ class Result extends StatelessWidget {
   Result(this.result, this.resetHandler);
 
   String get resultPhrase {
-    var resultText = "You did it \n😉";//added emoji so that it is more interactive 
+    var resultText =
+        "You did it \n😉"; //added emoji so that it is more interactive
     if (result >= 45) {
-      resultText = 'You are awesome! \n🤩';//for adding emojis press (window key) + (;)
+      resultText =
+          'You are awesome! \n🤩'; //for adding emojis press (window key) + (;)
     } else if (result > 20 && result < 45) {
       resultText = "Better luck next time! \n😊";
     } else {
@@ -28,14 +30,39 @@ class Result extends StatelessWidget {
             children: [
               SizedBox(height: 60.0),
               Text(
+                'Your score',
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.all(24.0),
+                padding: EdgeInsets.symmetric(vertical: 32.0),
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue[300],
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                ),
+                child: Text(
+                  result.toString(),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
                 resultPhrase,
                 style: TextStyle(
-                  fontSize: 36,
+                  fontSize: 28,
                   fontWeight: FontWeight.bold,
                 ),
                 textAlign: TextAlign.center,
               ),
-              SizedBox(height: 130.0),//added sized box so that it looks good ;)
+              SizedBox(height: 70.0), //added sized box so that it looks good ;)
               Padding(padding: EdgeInsets.only(top: 8)),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(primary: Colors.blue),
